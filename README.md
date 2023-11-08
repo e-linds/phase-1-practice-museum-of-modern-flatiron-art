c871658c5ecfbdf720c35c051a6da5a13577d38b
Emmi Lindsey <emmilindsey@gmail.com>
2023-11-08 09:44:04 -0700

    completed Museum of Flatiron Art code challenge

--- README.md
+++ README.md
@@ -14,13 +14,19 @@ Make a fetch to `http://localhost:3000/current-exhibits` and get the first exhib
 
 For each comment attached to the exhibit, add it to the `comments-section` with a `p` element.
 
+//fetch the data, and get the first item in the data array. Add element's info to the fitting elements on the page. For each comment, create a p element and append to comments-section. 
+
 ## Deliverable Two
 
 When someone submits the form for a new comment, that comment gets added to the `comments-section` as a `p` tag.
 
+create an event listener for the form which creates a new p tag and adds info to tag. 
+
 ## Deliverable Three
 
 When someone clicks the `buy-tickets-button` it updates the `tickets-bought` element so that it increments the number of tickets. Make sure to retain the text, it shouldn't just say `1` but instead say `1 Tickets Bought`.
+
+//create an event listener on buy-tickets-button to update tickets bought number. Likely need a parseInt and then plus
 
 ## BONUS One
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,79 @@
-console.log('Write your code here');
+console.log(
+
+    fetch(`http://localhost:3000/current-exhibits`)
+    .then(r => r.json())
+    .then(data => {
+
+    // populate the image, title, etc from database
+    const first = data[0]
+    document.querySelector("#exhibit-title").textContent = first.title
+    document.querySelector("#exhibit-image").src = first.image
+    document.querySelector("#tickets-bought").textContent = first.tickets_bought
+    document.querySelector("#exhibit-description").textContent = first.description
+
+   const array = first.comments
+
+    array.forEach((comment) => {
+        postComment(comment)
+   })
+
+   //event listener to submit new comments and update on front and back end 
+   const form = document.querySelector("#comment-form")
+   form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const addedComment = e.target["comment-input"].value
+    postComment(addedComment)
+    form.reset()  
+
+    // for fetch: create a new array to store all old comments and new ones
+    let newAllComments = []
+    array.forEach((oldComment) => {
+        newAllComments.push(oldComment)
+    })
+    newAllComments.push(addedComment)
+    
+    fetch('http://localhost:3000/current-exhibits/1', {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "comments": newAllComments
+        })
+   })
+
+})
+
+//event listener to increaase ticket count when ticket button is clicked, on front end and back end
+   const ticket_btn = document.querySelector("#buy-tickets-button")
+   ticket_btn.addEventListener("click", (e) => {
+    e.preventDefault()
+    let currentTicketNumber = parseInt(document.querySelector("#tickets-bought").textContent)
+    currentTicketNumber = currentTicketNumber + 1
+    document.querySelector("#tickets-bought").textContent = `${currentTicketNumber} tickets bought`
+
+    fetch('http://localhost:3000/current-exhibits/1', {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "tickets_bought": currentTicketNumber
+        })
+    })
+
+   })
+
+
+//helper function to add a comment, old or new, to the display
+   function postComment(new_comment) {
+    const p = document.createElement("p")
+        p.textContent = new_comment
+        document.querySelector("#comments-section").append(p)
+             }
+
+    })
+
+)
+
+
